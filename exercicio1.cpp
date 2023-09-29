6dcd80fdd54dcfb85f70f9d8ca9d46ee1d195381
leogmc <ifbaianoleo@gmail.com>
2023-09-29 00:09:42 -0300

    Enviando exercicio 1

---
+++ exercicio1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int calcularFatorial(int numero);
+
+int main()
+{
+    int numero;
+
+    cout << "Insira o número que deseja saber o fatorial: ";
+    cin >> numero;
+
+    cout << "O fatorial de " << numero << " é:" << calcularFatorial(numero);
+
+    return 0;
+}
+
+// Função fatorial
+int calcularFatorial(int numero)
+{
+
+    if (numero < 0)
+    {
+        cout << "Erro: O fatorial de um número negativo não está definido.";
+        return 0; // Retorna 0 em caso de erro
+    }
+
+    if (numero == 0)
+    {
+        cout << "O fatorial do número " << numero << " é: ";
+        return 1; // Retorna 1 caso o numero inserido seja 0
+    }
+
+    int fatoracao = 1;
+
+    for (int i = 1; i <= numero; i++)
+    {
+
+        fatoracao *= i;
+    }
+
+    return fatoracao;
+}
